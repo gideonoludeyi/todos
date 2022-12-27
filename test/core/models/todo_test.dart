15378daad14b55860d678bf9f980dd6760f97db9
gideonoludeyi <gooludeyi@gmail.com>
2022-12-26 20:36:28 -0500

    use TodoBuilder to simplify tests

--- test/core/models/todo_test.dart
+++ test/core/models/todo_test.dart
@@ -1,29 +1,18 @@
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:todos/core/models/todo.model.dart' show Todo;
+
+import '../../test_helper/builders/todo_builder.dart' show TodoBuilder;
 
 void main() {
   group("Todo completed status", () {
     test("A non-completed todo should have a status of 'In Progress'", () {
-      const todo = Todo(
-        id: "1",
-        title: "Todo#1",
-        description: "first",
-        completed: false,
-        userId: "0",
-      );
+      final Todo todo = TodoBuilder().asInProgress().build();
 
       expect(todo.status, equals("In Progress"));
     });
 
     test("A completed todo should have a status of 'Complete'", () {
-      const todo = Todo(
-        id: "1",
-        title: "Todo#1",
-        description: "first",
-        completed: true,
-        userId: "0",
-      );
+      final Todo todo = TodoBuilder().asCompleted().build();
 
       expect(todo.status, equals("Complete"));
     });
---
+++ test/test_helper/builders/todo_builder.dart
@@ -0,0 +1,69 @@
+import 'package:todos/core/models/todo.model.dart' show Todo;
+
+class TodoBuilder {
+  final String id;
+  final String title;
+  final String description;
+  final bool completed;
+  final String userId;
+
+  factory TodoBuilder() => TodoBuilder._(
+        id: "gid://Todo/0",
+        title: "the title",
+        description: "the description",
+        completed: false,
+        userId: "gid://User/0",
+      );
+
+  factory TodoBuilder.fromTodo(Todo todo) => TodoBuilder._(
+        id: todo.id,
+        title: todo.title,
+        description: todo.description,
+        completed: todo.completed,
+        userId: todo.userId,
+      );
+
+  TodoBuilder._({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.completed,
+    required this.userId,
+  });
+
+  TodoBuilder withId(String id) => copyWith(id: id);
+
+  TodoBuilder withTitle(String title) => copyWith(title: title);
+
+  TodoBuilder withDescription(String description) =>
+      copyWith(description: description);
+
+  TodoBuilder asCompleted() => copyWith(completed: true);
+
+  TodoBuilder asInProgress() => copyWith(completed: false);
+
+  TodoBuilder byUser(String userId) => copyWith(userId: userId);
+
+  Todo build() => Todo(
+        id: id,
+        title: title,
+        description: description,
+        completed: completed,
+        userId: userId,
+      );
+
+  TodoBuilder copyWith({
+    id,
+    title,
+    description,
+    completed,
+    userId,
+  }) =>
+      TodoBuilder._(
+        id: id ?? this.id,
+        title: title ?? this.title,
+        description: description ?? this.description,
+        completed: completed ?? this.completed,
+        userId: userId ?? this.userId,
+      );
+}
