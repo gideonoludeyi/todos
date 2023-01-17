720c891a8745ecf1b4650e02663eabe3a8eb2967
gideonoludeyi <gooludeyi@gmail.com>
2023-01-17 11:18:26 -0500

    fix keyboard overlapping AddTodoForm modal

--- lib/views/components/add_todo_form/show_add_todo_form_modal.dart
+++ lib/views/components/add_todo_form/show_add_todo_form_modal.dart
@@ -8,8 +8,7 @@ Future<void> showAddTodoFormModal({
   required BuildContext context,
   required void Function(AddTodoFormResponse response)? onSubmit,
 }) async {
-  final AddTodoFormResponse? response =
-      await showModalBottomSheet<AddTodoFormResponse>(
+  final response = await _showModalBottomSheetCustom(
     context: context,
     builder: (context) => AddTodoForm(
       onSubmit: (response) =>
@@ -21,3 +20,21 @@ Future<void> showAddTodoFormModal({
     onSubmit?.call(response);
   }
 }
+
+/// wrapper around showModalBottomSheet
+/// to resolve overlapping keyboard issue
+Future<T?> _showModalBottomSheetCustom<T>({
+  required BuildContext context,
+  required Widget Function(BuildContext) builder,
+}) =>
+    showModalBottomSheet<T>(
+      isScrollControlled: true,
+      context: context,
+      builder: (context) => SingleChildScrollView(
+        child: Container(
+          padding:
+              EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+          child: builder(context),
+        ),
+      ),
+    );
